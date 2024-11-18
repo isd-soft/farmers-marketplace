9a55df16f890cac4449f5a6a757d695cd5746c02
MaryLens <marycravts@gmail.com>
2024-11-18 14:18:36 +0200

    Initial project created

---
+++ isd-farmers-market/src/main/java/com/example/isdfarmersmarket/IsdFarmersMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.isdfarmersmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsdFarmersMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsdFarmersMarketApplication.class, args);
+	}
+
+}
---
+++ isd-farmers-market/src/test/java/com/example/isdfarmersmarket/IsdFarmersMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isdfarmersmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsdFarmersMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
