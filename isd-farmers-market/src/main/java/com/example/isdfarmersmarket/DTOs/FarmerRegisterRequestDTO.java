3ba0befeb6983c3411d189c44c004386593164de
hyrremm <abyssinianconq@gmail.com>
2024-11-20 12:28:02 +0200

    fixed FarmerRegisterRequestDTO

--- isd-farmers-market/src/main/java/com/example/isdfarmersmarket/DTOs/FarmerRegisterRequestDTO.java
+++ isd-farmers-market/src/main/java/com/example/isdfarmersmarket/DTOs/FarmerRegisterRequestDTO.java
@@ -1,11 +1,12 @@
 package com.example.isdfarmersmarket.DTOs;
 
+import com.example.isdfarmersmarket.DTOs.abstractions.UserRegisterRequestDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class FarmerRegisterRequestDTO extends CustomerRegisterRequestDTO {
+public class FarmerRegisterRequestDTO extends UserRegisterRequestDTO {
     private String address;
     private String description;
 }
