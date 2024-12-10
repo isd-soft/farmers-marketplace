7b7df5b56882c23e500bc02c225bd69945ec53fc
Inesa Godorozea <Inesagodorozea@gmail.com>
2024-12-10 12:32:50 +0200

    Added delivery_type_id column in planned_orders table

--- isd-farmers-market/src/main/resources/db/migration/V12__create_planned_orders_table.sql
+++ isd-farmers-market/src/main/resources/db/migration/V12__create_planned_orders_table.sql
@@ -2,11 +2,13 @@ CREATE TABLE planned_orders (
                                 id SERIAL PRIMARY KEY,
                                 customer_id BIGINT NOT NULL,
                                 product_id BIGINT NOT NULL,
+                                delivery_type_id BIGINT NOT NULL,
                                 quantity INTEGER NOT NULL,
                                 day_of_week VARCHAR(9),
                                 time TIME,
                                 active BOOLEAN DEFAULT FALSE,
                                 created_date TIMESTAMP,
                                 CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES users(id) ON DELETE CASCADE,
-                                CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+                                CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+                                CONSTRAINT fk_delivery_type FOREIGN KEY (delivery_type_id) REFERENCES delivery_type_farmer(id) ON DELETE CASCADE
 );
