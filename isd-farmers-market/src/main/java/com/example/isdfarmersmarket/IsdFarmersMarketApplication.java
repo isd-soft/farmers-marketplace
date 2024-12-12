ac618fadf209ef5c7b89f00728d6502f40e180eb
hyrremm <abyssinianconq@gmail.com>
2024-12-12 17:35:44 +0200

    timezone fixed

--- isd-farmers-market/src/main/java/com/example/isdfarmersmarket/IsdFarmersMarketApplication.java
+++ isd-farmers-market/src/main/java/com/example/isdfarmersmarket/IsdFarmersMarketApplication.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class IsdFarmersMarketApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Chisinau"));
 		SpringApplication.run(IsdFarmersMarketApplication.class, args);
 	}
 
